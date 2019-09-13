c902bc7b59bcff8613517402e55133c6fb8362c3
Willian <wiliam-m-@hotmail.com>
2019-09-13 01:35:41 -0300

    Jogo da Velha

---
+++ classes/JogoDaVelha.py
@@ -0,0 +1,47 @@
+import numpy as np
+from os import system
+
+class JogoDaVelha:
+    def __init__(self):
+        self.__player_1 = None
+        self.__player_2 = None
+        self.array = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']], dtype=np.str)
+
+    def jogar(self):
+        self.__player_1 = input('Digite nome do jogador 1: ')
+        self.__player_2 = input('Digite nome do jogador 2: ')
+        system('clear')
+        self.printa()
+        p = 9
+        while(p):
+            if p % 2 != 0:
+                x, y = input('Digite a posicao: ').split(' ')
+                x = int(x)
+                y = int(y)
+                self.player_1(x,y)
+            else:
+                x, y = input('Digite a posicao: ').split(' ')
+                x = int(x)
+                y = int(y)
+                self.player_2(x, y)
+            p -= 1
+            self.printa()
+
+
+    def printa(self):
+
+        print(f'  {self.array[0,0]}  |  {self.array[0,1]}  |  {self.array[0,2]}  ')
+        print('_____|_____|_____')
+        print('     |     |     ')
+        print(f'  {self.array[1,0]}  |  {self.array[1,1]}  |  {self.array[1,2]}  ')
+        print('_____|_____|_____')
+        print('     |     |     ')
+        print(f'  {self.array[2,0]}  |  {self.array[2,1]}  |  {self.array[2,2]}  ')
+
+    def player_1(self, x, y):
+        self.x, self.y = x,y
+        self.array[self.x, self.y] = 'X'
+
+    def player_2(self, x, y):
+        self.x, self.y = x,y
+        self.array[self.x, self.y] = 'O'
---
+++ main.py
@@ -0,0 +1,5 @@
+from classes.JogoDaVelha import JogoDaVelha
+
+Jogo = JogoDaVelha()
+
+Jogo.jogar()
